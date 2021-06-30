f406e64143140ff2ddd05b09307a03dd17110b70
akitaka <syugen3619@gmail.com>
2021-06-30 14:05:29 +0000

    Mentor

---
+++ mentor.rb
@@ -0,0 +1,24 @@
+class Mentor
+  attr_accessor :name
+  
+  def initialize(name)
+    self.name = name
+  end
+  
+  def job
+    puts "#{self.name}です。私は現役のITプロフェッショナルです。"
+  end
+end
+
+class RailsMentor < Mentor
+  
+  def job
+   puts "#{self.name}です。私はRubyとRailsでwebアプリケーションを作ります。"
+  end
+end
+
+kirameki_A = Mentor.new("煌")
+akaide = RailsMentor.new("赤出")
+
+kirameki_A.job
+akaide.job
